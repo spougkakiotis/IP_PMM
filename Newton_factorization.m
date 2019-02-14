752b04b1f6ff4265ac44c286fe1b4b447496a639
spougkakiotis <33895657+spougkakiotis@users.noreply.github.com>
2019-02-14 14:32:29 +0000

    Create Newton_factorization.m

---
+++ Newton_factorization.m
@@ -0,0 +1,40 @@
+function NS = Newton_factorization(A,A_tr,Q,x,z,delta,rho,pos_vars,free_vars)
+% ==================================================================================================================== %
+% Newton_factorization: Factorize the Newton matrix
+% -------------------------------------------------------------------------------------------------------------------- %
+% NS = Newton_factorization(A,A_tr,Q,x,z,delta,rho,pos_vars,free_vars) returns a MATLAB struct that holds the
+%      factorization of the Newton matrix for solving the step equations in
+%      the IPM, as well as relevant information concerning failure.
+% Factorization Method
+  % --------------------
+  % 1: augmented system, LDL' factorization.
+% ==================================================================================================================== %
+[m, n] = size(A);
+NS = struct();
+% ==================================================================================================================== %
+% LDL' factorization of KKT matrix
+% -------------------------------------------------------------------------------------------------------------------- %
+% Perform the same reduction as above but calculate its symmetric
+% indefinite factorization
+%
+%   K(pp,pp) = L*D*L'.
+%
+% MATLAB uses MA57 when K is sparse, which is not available in OCTAVE. 
+% -------------------------------------------------------------------------------------------------------------------- %
+NS.x = x;
+NS.z = z;
+Q_bar = zeros(n,1);
+if (size(pos_vars,1) > 0)
+    Q_bar(pos_vars) = z(pos_vars)./x(pos_vars) + rho;
+    Q_bar(free_vars) = rho;
+else
+    Q_bar(:) = rho;
+end
+K = [-Q-spdiags(Q_bar,0,n,n), A_tr; A, delta.*speye(m,m)]; 
+[NS.L,NS.D,NS.pp] = ldl(K,10^(-12),'vector'); %Small pivots allowed, to avoid 2x2 pivots.
+% ==================================================================================================================== %  
+ 
+% ******************************************************************************************************************** %
+% END OF FILE.
+% ******************************************************************************************************************** %
+end
